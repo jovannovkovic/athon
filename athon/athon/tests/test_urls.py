b19550adec624f02e15cd35fe02bfeda0f65c853
Jovan Novkovic <jovan_novko@yahoo.com>
2015-02-15 16:50:22 +0100

    Add more tests.

--- athon/athon/tests/test_urls.py
+++ athon/athon/tests/test_urls.py
@@ -187,14 +187,148 @@ def test_fallow_non_existing_user(logged_client, another_user):
 def test_unfallow_user(logged_client, another_user):
     a_user = get_user_model().objects.get(username=TEST_USERNAME).athon_user
     an_user = another_user.athon_user
+    models.FallowUsers.objects.create(follower=a_user, followed_user=an_user)
+    a_user.fallowing_number = 1
+    a_user.save()
+    an_user.fallowers_number = 1
+    an_user.save()
+    assert a_user.fallowing.filter(followed_user=another_user.athon_user).count() == 1
     response = logged_client.post(
-        "/api/user/fallow/",
+        "/api/user/unfallow/",
         {
-            'followed_user': another_user.pk
+            'id': another_user.pk
         }, format='json')
 
-    assert a_user.fallowing.filter(followed_user=another_user).count() == 1
+    assert a_user.fallowing.filter(followed_user=another_user.athon_user).count() == 0
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_request_to_fallow_user(logged_client, another_user):
+    another_user.athon_user.is_public_profile = False
+    another_user.athon_user.save()
+    response = logged_client.post(
+        "/api/user/request_to_fallow/",
+        {
+            'id': another_user.pk
+        }, format='json')
+
+    assert another_user.athon_user.fallowers.filter(request_status=True).count() == 1
     assert response.status_code == status.HTTP_201_CREATED
+    a_user = get_user_model().objects.get(username=TEST_USERNAME).athon_user
+    assert a_user.fallowing_number == 0
+
+
+@pytest.mark.django_db
+def test_request_to_fallow_public_user(logged_client, another_user):
+    another_user.athon_user.is_public_profile = True
+    another_user.athon_user.save()
+    response = logged_client.post(
+        "/api/user/request_to_fallow/",
+        {
+            'id': another_user.pk
+        }, format='json')
+    assert another_user.athon_user.fallowers.filter(request_status=True).count() == 0
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    a_user = get_user_model().objects.get(username=TEST_USERNAME).athon_user
+    assert a_user.fallowing_number == 0
+
+
+@pytest.mark.django_db
+def test_request_to_fallow_non_existing_user(logged_client, another_user):
+    another_user.athon_user.is_public_profile = False
+    another_user.athon_user.save()
+    user_id = another_user.pk + 1
+    response = logged_client.post(
+        "/api/user/request_to_fallow/",
+        {
+            'id': user_id
+        }, format='json')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_remove_request_to_fallow_user(logged_client, another_user):
+    a_user = get_user_model().objects.get(username=TEST_USERNAME).athon_user
+    models.FallowUsers.objects.create(follower=a_user,
+            followed_user=another_user.athon_user, request_status=True)
+    assert another_user.athon_user.fallowers.filter(request_status=True).count() == 1
+    response = logged_client.post(
+        "/api/user/remove_request_to_fallow/",
+        {
+            'id': another_user.pk
+        }, format='json')
+
+    assert another_user.athon_user.fallowers.filter(request_status=True).count() == 0
+    assert response.status_code == status.HTTP_200_OK
+    a_user = get_user_model().objects.get(username=TEST_USERNAME).athon_user
+    assert a_user.fallowing_number == 0
+
+
+@pytest.mark.django_db
+def test_remove_request_to_fallow_not_existing_relationship(logged_client, another_user):
+    another_user.athon_user.is_public_profile = True
+    another_user.athon_user.save()
+    response = logged_client.post(
+        "/api/user/remove_request_to_fallow/",
+        {
+            'id': another_user.pk
+        }, format='json')
+    assert another_user.athon_user.fallowers.filter(request_status=True).count() == 0
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_remove_request_to_fallow_non_existing_user(logged_client, another_user):
+    user_id = another_user.pk + 1
+    response = logged_client.post(
+        "/api/user/remove_request_to_fallow/",
+        {
+            'id': user_id
+        }, format='json')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_accept_request_to_fallow_user(logged_client, another_user):
+    a_user = get_user_model().objects.get(username=TEST_USERNAME).athon_user
+    models.FallowUsers.objects.create(follower=another_user.athon_user,
+            followed_user=a_user, request_status=True)
+    assert a_user.fallowers.filter(request_status=True).count() == 1
+    response = logged_client.post(
+        "/api/user/accept_request/",
+        {
+            'id': another_user.pk
+        }, format='json')
+
+    assert a_user.fallowers.filter(request_status=True).count() == 0
+    assert response.status_code == status.HTTP_200_OK
+    a_user = get_user_model().objects.get(username=TEST_USERNAME).athon_user
+    assert a_user.fallowers_number == 1
+    another_user = get_user_model().objects.get(id=another_user.pk).athon_user
+    assert another_user.fallowing_number == 1
+
+
+@pytest.mark.django_db
+def test_accept_request_to_fallow_not_existing_relationship(logged_client, another_user):
+    response = logged_client.post(
+        "/api/user/accept_request/",
+        {
+            'id': another_user.pk
+        }, format='json')
+    assert another_user.athon_user.fallowers.filter(request_status=True).count() == 0
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_accept_request_to_fallow_non_existing_user(logged_client, another_user):
+    user_id = another_user.pk + 1
+    response = logged_client.post(
+        "/api/user/accept_request/",
+        {
+            'id': user_id
+        }, format='json')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 
